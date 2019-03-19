e4b9f9dd5768238c68d550ec22774f7d2ffaa1bf
Anna Priliko <anna.prilipkoo@gmail.com>
2019-03-19 22:42:48 +0200

    new

---
+++ task08/task0821/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task08.task0821;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Однофамильцы и тёзки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> map = createPeopleList();
+        printPeopleList(map);
+    }
+
+    public static Map<String, String> createPeopleList() {
+
+        HashMap<String,String> map = new HashMap<String, String>();
+
+        map.put("Vasiliev","Vasia");
+        map.put("Vasiliev","Vasia");
+        map.put("Vasiliev","Vasia");
+        map.put("Vasiliev","Vasia");
+        map.put("Vasiliev","Vasia");
+        map.put("Vasiliev","Vasia");
+        map.put("Vasiliev","Vasia");
+        map.put("Vasiliev","Vasia");
+        map.put("Vasiliev","Vasia");
+        map.put("Vasiliev","Vasia");
+        //напишите тут ваш код
+
+        return map;
+    }
+
+    public static void printPeopleList(Map<String, String> map) {
+        for (Map.Entry<String, String> s : map.entrySet()) {
+            System.out.println(s.getKey() + " " + s.getValue());
+        }
+    }
+}
